6d3a5bebcb1c6c42413ec4e5b7897df514e09bbd
Jake Lee <jkjalee@gmail.com>
2025-05-03 23:58:48 -0700

    e2eTest progress

---
+++ e2eTest.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+driver_path = ChromeDriverManager().install()
+service = Service(driver_path)
+driver = webdriver.Chrome(service=service)
+driver.maximize_window()
+driver.implicitly_wait(5)
+driver.wait = WebDriverWait(driver, timeout=10)
+
+driver.get('https://rahulshettyacademy.com/angularpractice/')
+
+driver.find_element(By.CSS_SELECTOR, "[href='/angularpractice/shop']")
+
+checkout_btn = driver.find_element(By.CSS_SELECTOR, "#navbarResponsive a")
+checkout_btn_counter = checkout_btn.text
+
+cards = driver.find_elements(By.CSS_SELECTOR, ".card.h-100")
+
+for card in cards:
+    if card.find_element(By.CSS_SELECTOR, "h4 a").text.strip().lower() == 'iphone x':
+        card.find_element(By.CSS_SELECTOR, "button").click()
+
+checkout_btn_counter_updated = checkout_btn.text
+
+assert checkout_btn_counter != checkout_btn_counter_updated, "Checkout button counter has not been updated"
+
+checkout_btn.click()
+
+driver.find_element(By.CSS_SELECTOR, ".btn.btn-success").click()
+
+country = 'united states of america'
+country_input = driver.find_element(By.ID, "country")
+country_input.send_keys(country)
+
+country_dropdown_selection = (By.XPATH, "//a[text()='United States of America']")
+
+driver.wait.until(
+    EC.element_to_be_clickable(country_dropdown_selection),
+        message=f'Element not clickable by {country_dropdown_selection}'
+    )
+
+
+
+
+
+
+
